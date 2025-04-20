1a7175cebd2a406ba56835dcbe8713ceaaa8de36
Jaylen Dixon <jaylendixon08@gmail.com>
2025-04-19 21:29:33 -0700

    app.js

--- be/e-com/app.js
+++ be/e-com/app.js
@@ -25,6 +25,7 @@ const corsOption = {
 app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 mongoose
   .connect(mongoURI)
